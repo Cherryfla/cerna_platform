82c84316196f6b6f50461e1f2fa84861b3c988d5
CherryFla <843479000@qq.com>
2020-04-05 16:12:39 +0800

    deploy frontend

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+/*项目发布阶段用到的插件babel*/
+const prodPlugin = []
+
+if(process.env.NODE_ENV === 'production'){
+  prodPlugin.push('transform-remove-console')
+}
+
 module.exports = {
   'presets': [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +16,8 @@ module.exports = {
         'libraryName': 'element-ui',
         'styleLibraryName': 'theme-chalk'
       }
-    ]
+    ],
+    //发布产品阶段的插件数组
+    ...prodPlugin
   ]
 }
--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,7 @@ module.exports = {
       }
     }
   },
+  //配置favicon
   pwa: {
     iconPaths: {
       favicon32: 'favicon.ico',
@@ -22,18 +23,44 @@ module.exports = {
     }
   },
   chainWebpack: (config) => {
-  // 删除之前所有svg规则
-  config.module.rules.delete("svg");
-  config.module
-    .rule('svg')
-    .test(/\.svg$/)
-    .include
-    .add(resolve('src/icons/svg'))  //指定.svg存放目录,对于不需要指定的.svg可以使用exclude
-    .end()
-    .use('svg-sprite-loader')
-    .loader('svg-sprite-loader')
-    .options({
-      symbolId: 'icon-[name]'  // 规定每个图元的id,可修改,只要use时是#id的形式即可
+    //发布模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        echarts: 'echarts',
+        nprogress: 'NProgress'
+      })
+
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
     })
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
+    })
+    // 删除之前所有svg规则
+    config.module.rules.delete("svg");
+    config.module
+      .rule('svg')
+      .test(/\.svg$/)
+      .include
+      .add(resolve('src/icons/svg'))  //指定.svg存放目录,对于不需要指定的.svg可以使用exclude
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'  // 规定每个图元的id,可修改,只要use时是#id的形式即可
+      })
   }
 }
